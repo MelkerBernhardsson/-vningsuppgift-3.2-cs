980c6d8761ac3fc0211b47b93d0327b8b908919e
Pingvu <nhlpool27@gmail.com>
2023-02-03 09:05:45 +0100

    Add project files.

---
+++ övningsuppgift 3.2 cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vem vann vm i fotboll för damer 2015?");
+            string svar = Console.ReadLine();
+            if(svar == "usa" )
+            {
+                Console.WriteLine("du svarade rätt!");
+            }
+            else
+            {
+                Console.WriteLine("fel svar :( ");
+            }
+        }
+    }
+}
